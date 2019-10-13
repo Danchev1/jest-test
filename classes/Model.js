2fb9a02ecfaaa883bd98dbe344918611ba69523c
yordandanchev <yordangdanchev@gmail.com>
2019-10-13 21:40:13 +0300

    Jest playground - class update, tests does not pass [... for later]

--- classes/Model.js
+++ classes/Model.js
@@ -1,22 +1,28 @@
 export default class Model {
   constructor(data = []) {
-    this.$collection = data;
+    this.$collection = this.record(data);
   }
 
   record(item) {
-    Array.isArray(item) ? this.$collection.push(...item) : this.$collection.push(item);
+    Array.isArray(item) || this.$collection.push(...item);
+    Object.prototype.toString.call(item) === '[object Object]' || this.$collection.push(item);
   }
 
   getAll() {
-    return this.$collection.map(item => item);
+    return this.$collection.map(item => this.bufferCopy(item));
   }
 
-  find(item) {
-    return this.$collection.find(record => record === item);
+  find(id) {
+    let found = this.$collection.find(record => record.id === id);
+    return found ? this.bufferCopy(found) : null;
   }
 
-  update(item, index) {
-    return this.$collection[index] = item;
+  update(id, newValue) {
+    return this.find(id).value = newValue;
+  }
+
+  bufferCopy(item) {
+    return JSON.parse(JSON.stringify(item))
   }
 
 }
